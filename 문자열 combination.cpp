9f571b3819d3298917ecb8c56a10ca1a78dc8e2a
GangsterAnt <49489178+GangsterAnt@users.noreply.github.com>
2020-09-13 09:42:40 +0900

    Update 문자열 combination.cpp

--- 문자열 combination.cpp
+++ 문자열 combination.cpp
@@ -1,5 +1,7 @@
+//디폴트 매개변수 temp 와 k 를 사용하므로써 외부벡터 combination 에 깊이 depth 만큼의 dfs 를 통해 조합을 만들어준다.
+
 void Combination(int k = 0, string temp = "") {
-    for (int i = k; i < column; i++) {
+    for (int i = k; i < depth; i++) {
         temp += to_string(i);
         combination.push_back(temp);
         Combination(i + 1, temp);
